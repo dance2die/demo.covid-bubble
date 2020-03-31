69173a2a014deb29d9280f2a7b42d7553db31e23
Sung M. Kim <sungmkim80@gmail.com>
2020-03-30 22:51:38 -0400

    Added dropdown for color change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ResponsiveBubble as Bubble } from '@nivo/circle-packing'
+import create from 'zustand'
 // import { Bubble } from '@nivo/circle-packing'
 // import { ResponsiveBubbleHtml  as Bubble } from '@nivo/circle-packing'
 // import { ResponsiveBubbleCanvas   as Bubble } from '@nivo/circle-packing'
@@ -20,6 +21,52 @@ type Stats = {
 }
 
 type ByCountry = (data: any) => any
+
+
+const [useColors] = create(set => ({
+  currentColor: 'nivo',
+  colors: [
+    'nivo',
+    'category10',
+    'accent',
+    'dark2',
+    'paired',
+    'pastel1',
+    'pastel2',
+    'set1',
+    'set2',
+    'set3',
+    'brown_blueGreen',
+    'purpleRed_green',
+    'pink_yellowGreen',
+    'purple_orange',
+    'red_blue',
+    'red_grey',
+    'red_yellow_blue',
+    'red_yellow_green',
+    'spectral',
+    'blues',
+    'greens',
+    'greys',
+    'oranges',
+    'purples',
+    'reds',
+    'blue_green',
+    'blue_purple',
+    'green_blue',
+    'orange_red',
+    'purple_blue_green',
+    'purple_blue',
+    'purple_red',
+    'red_purple',
+    'yellow_green_blue',
+    'yellow_green',
+    'yellow_orange_brown',
+    'yellow_orange_red'
+  ],
+  setColor: (newColor: string) => set(state => ({ currentColor: newColor }))
+}))
+
 
 const getStatsChildren = (stats: Stats) => {
   const result = []
@@ -93,84 +140,111 @@ const normalize = (data: any) => {
   }
 }
 
-const MyResponsiveBubble = ({ root }: any) => (
-  <Bubble
-    // width={800}
-    // height={800}
-    root={root}
-    margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
-    identity='name'
-    value='count'
-    // colors={{ scheme: 'nivo' }}
-    colors={{ scheme: 'purple_blue_green' }}
-    // colorBy='name'
-    colorBy='depth'
-    padding={6}
-    // labelTextColor={{ from: 'color', modifiers: [['darker', 0.8]] }}
-    labelTextColor="black"
-    borderWidth={2}
-    borderColor={{ from: 'color' }}
-    defs={[
-      {
-        id: 'lines',
-        type: 'patternLines',
-        background: 'none',
-        color: 'inherit',
-        rotation: -45,
-        lineWidth: 5,
-        spacing: 8
-      }
-    ]}
-    fill={[{ match: { depth: 1 }, id: 'lines' }]}
-    animate={true}
-    motionStiffness={66}
-    motionDamping={12}
-    tooltip={({
-      id,
-      value,
-      color,
-      depth,
-      path,
-      parent,
-      ...rest
-    }: {
-      id: number
-      value: string
-      color: string
-      depth: number
-      path: string
-      parent: any
-      rest: any
-    }) => {
-      // console.info(`rest`, rest, id, value, color)
-      // console.info(`parent, depth, path`, parent, depth, path, id, value, color)
+const MyResponsiveBubble = ({ root }: any) => {
+  const {currentColor} = useColors()
 
-      return (
-        <strong style={{ color }}>
-          {/* {id}: {value} */}
-          {depth > 0
-            ? path
-                .split('.')
-                .reverse()
-                .slice(1)
-                .join('/')
-            : id}
-          : {value}
-        </strong>
-      )
-    }}
-  />
-)
+  useEffect(() => {
+    console.info(`currentColor ==>`, currentColor)
+  })
+
+  return (
+    <Bubble
+      // width={800}
+      // height={800}
+      root={root}
+      margin={{ top: 20, right: 20, bottom: 20, left: 20 }}
+      identity='name'
+      value='count'
+      // colors={{ scheme: 'nivo' }}
+      colors={{ scheme: currentColor }}
+      // colors={{ scheme: 'brown_blueGreen' }}
+      // colorBy='name'
+      colorBy='depth'
+      padding={6}
+      // labelTextColor={{ from: 'color', modifiers: [['darker', 0.8]] }}
+      labelTextColor='black'
+      borderWidth={2}
+      borderColor={{ from: 'color' }}
+      defs={[
+        {
+          id: 'lines',
+          type: 'patternLines',
+          background: 'none',
+          color: 'inherit',
+          rotation: -45,
+          lineWidth: 5,
+          spacing: 8
+        }
+      ]}
+      fill={[{ match: { depth: 1 }, id: 'lines' }]}
+      animate={true}
+      motionStiffness={66}
+      motionDamping={12}
+      tooltip={({
+        id,
+        value,
+        color,
+        depth,
+        path,
+        parent,
+        ...rest
+      }: {
+        id: number
+        value: string
+        color: string
+        depth: number
+        path: string
+        parent: any
+        rest: any
+      }) => {
+        // console.info(`rest`, rest, id, value, color)
+        // console.info(`parent, depth, path`, parent, depth, path, id, value, color)
+  
+        return (
+          <strong style={{ color }}>
+            {/* {id}: {value} */}
+            {depth > 0
+              ? path
+                  .split('.')
+                  .reverse()
+                  .slice(1)
+                  .join('/')
+              : id}
+            : {value}
+          </strong>
+        )
+      }}
+    />
+  )
+}
 
 function App() {
   const data = normalize(bycountry)
+  const { colors, setColor } = useColors()
 
   // console.log(`data =>>`, data)
 
   // return <h1>hi</h1>
 
+  const handleColorSelect = (e: React.SyntheticEvent<HTMLSelectElement>) => {
+    e.persist();
+    console.info(`select value ===>>>`, e.currentTarget.value)
+    setColor(e.currentTarget.value)
+  }
+
   return (
     <div className='flex flex-col h-full items-center justify-center bg-gray-900 text-white'>
+      <select
+        onChange={handleColorSelect}
+        className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+        id='grid-state'
+      >
+        {colors.map((color: string) => (
+          <option key={color} value={color}>
+            {color}
+          </option>
+        ))}
+      </select>
       <MyResponsiveBubble root={data} />
     </div>
   )
